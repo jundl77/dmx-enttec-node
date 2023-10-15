63610c021f61392ff68bcde8d610c7c4079a6d49
Julian Brendl <julianbrendl@gmail.com>
2023-10-15 21:07:08 +0200

    adjusted comments

--- libs/core/clock.cc
+++ libs/core/clock.cc
@@ -11,11 +11,11 @@ uint64_t rdtscp()
 {
 #if defined(WIN32) // WINDOWS
     return GetTickCount64();
-#elif defined(__arm64__) // OSX ARM based
+#elif defined(__arm64__) // ARM64
     uint64_t val;
     __asm__ __volatile__("mrs %0, cntvct_el0" : "=r" (val));
     return val;
-#else // LINUX
+#else // x86-64
     uint64_t rax, rcx, rdx;
 	__asm__ __volatile__("rdtscp" : "=a"(rax), "=d"(rdx), "=c"(rcx));
 	return (rdx << 32) + rax;
@@ -26,9 +26,9 @@ void cpuid()
 {
 #if defined(WIN32) // WINDOWS
     // TODO: impl for windows
-#elif defined(__arm64__) // OSX ARM based
+#elif defined(__arm64__) // ARM64
     // TODO: impl for mac
-#else // LINUX
+#else // x86-64
     uint64_t rax, rbx, rcx, rdx;
     __asm__ __volatile__("cpuid" : "=a"(rax), "=b"(rbx), "=d"(rdx), "=c"(rcx));
 #endif
@@ -39,12 +39,12 @@ uint64_t rdtscp(int& chip, int& core)
 #if defined(WIN32) // WINDOWS
     // TODO: revise, this probably does not work
     return GetTickCount64();
-#elif defined(__arm64__) // OSX ARM based
+#elif defined(__arm64__) // ARM64
     // TODO: revise, this probably does not work
     uint64_t val;
     __asm__ __volatile__("mrs %0, cntvct_el0" : "=r" (val));
     return val;
-#else // LINUX
+#else // x86-64
     uint64_t rax, rcx, rdx;
 	__asm__ __volatile__("rdtscp" : "=a"(rax), "=d"(rdx), "=c"(rcx));
 	chip = static_cast<int>((rcx & 0xFFF000) >> 12);
